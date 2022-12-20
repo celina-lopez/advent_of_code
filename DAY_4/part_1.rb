a378638a7d3c8dbdc25707a99ee6c9a3b2b2c605
Celina Lopez <marisa.celina.lopez4@gmail.com>
2022-12-20 15:11:39 -0800

    day 4

---
+++ DAY_4/part_1.rb
@@ -0,0 +1,20 @@
+file_data = File.open("data.txt").readlines.map(&:chomp)
+
+total = 0 
+
+def find_cleaning_chores(data)
+  first_num,second_num = data.split("-")
+  (first_num..second_num).to_a
+end
+
+file_data.each do |data|
+  first_elf,second_elf = data.split(",")
+  first_elf_cleaning_chores = find_cleaning_chores(first_elf)
+  second_elf_cleaning_chores = find_cleaning_chores(second_elf)
+  overlapping = (first_elf_cleaning_chores & second_elf_cleaning_chores)
+  if overlapping == first_elf_cleaning_chores || overlapping == second_elf_cleaning_chores
+    total += 1
+  end
+end;nil
+
+puts total
